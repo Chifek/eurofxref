9db2191ca188b47313558a71d25a0ef6b50748dd
Ruslan Ismailov <ruslan@mavix.io>
2019-03-18 19:43:20 +0600

    Deleted not used files

--- tests/Cbr/Command/CbrCmdTest.php
+++ tests/Cbr/Command/CbrCmdTest.php
@@ -2,7 +2,6 @@
 
 namespace App\Tests\Cbr\Command;
 
-use App\Command\CbrCmd;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
